0d80ac1abe8b331d8fca629862ebfe383fbea539
ruslyc <ruslyc@yahoo.com>
2020-10-20 19:08:15 -0500

    DI Setter Injection

---
+++ Spring-02-DI-SetterInjection/src/main/java/services/Selenium.java
@@ -0,0 +1,9 @@
+package services;
+
+import interfaces.Course;
+
+public class Selenium implements Course {
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours: 30");
+    }
+}
