0c916d9dbf5cd999fffb593195d11305a9efe1c6
ruslyc <ruslyc@yahoo.com>
2020-10-27 18:36:30 -0500

    Java Annotations Configuration Post Construct and Pre Destroy

---
+++ Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/CybertekApp.java
@@ -0,0 +1,20 @@
+import config.CybertekAppConfig;
+import interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class CybertekApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext(CybertekAppConfig.class);
+
+        Course course = container.getBean("java",Course.class);
+
+        course.getTeachingHours();
+
+        ((AnnotationConfigApplicationContext)container).close();
+
+
+    }
+}
---
+++ Spring-06-JavaCode-PostConstruct-PreDestroy/src/main/java/services/Selenium.java
@@ -0,0 +1,10 @@
+package services;
+
+import interfaces.Course;
+
+public class Selenium implements Course {
+
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours: 30");
+    }
+}
